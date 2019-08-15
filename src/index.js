1b6abb401a9a50a8b037e048b25221d44a7e1f3a
cereallarceny <me@patrickcason.com>
2019-08-15 15:13:41 +0100

    Working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/*
+TODO:
+- https://blog.jscrambler.com/scaling-node-js-socket-server-with-nginx-and-redis/
+*/
+
 import WebSocket from 'ws';
 
 import initDB from './init-db';
@@ -24,53 +29,10 @@ const startSocketServer = async () => {
       const send = (type, data, socket = ws) =>
         socket.send(JSON.stringify({ type, data }));
 
+      console.log(type, data);
+
       if (type.includes('webrtc:')) {
-        if (type === 'webrtc: join-room') {
-          // If the room doesn't exist yet, create it
-          if (!rooms.hasOwnProperty(data.scopeId)) {
-            rooms[data.scopeId] = {};
-          }
-
-          // Have the current client join the room
-          rooms[data.scopeId][data.instanceId] = ws;
-
-          // Let everyone else in the room know about the new client
-          Object.keys(rooms[data.scopeId]).forEach(client => {
-            send(
-              'webrtc: new-peer',
-              { instanceId: data.instanceId },
-              rooms[data.scopeId][client]
-            );
-          });
-        } else if (type === 'webrtc: internal-message') {
-          if (
-            data.to !== undefined &&
-            rooms[data.scopeId][data.to] !== undefined
-          ) {
-            // If the message indicates the recipient and this recipient is known to the server, we send the message only to him...
-            send(
-              'webrtc: internal-message',
-              data,
-              rooms[data.scopeId][data.to]
-            );
-          } else {
-            // ... otherwise we consider the message to be broadcast
-            Object.keys(rooms[data.scopeId]).forEach(client => {
-              if (client !== data.instanceId) {
-                send(
-                  'webrtc: internal-message',
-                  data,
-                  rooms[data.scopeId][client]
-                );
-              }
-            });
-          }
-        } else if (type === 'webrtc: peer-disconnected') {
-          // TODO
-          // When disconnecting a client, we inform the rest about it
-          // socket.broadcast.to(socket.room).emit('leave', socket.user_id);
-          // delete users[socket.user_id];
-        }
+        runWebRTC(type, data, send, rooms, ws);
       } else if (type === 'get-protocol') {
         const protocol = await getProtocol(db, data);
 
