1033169a9f5b03e6afe4946238514d2d9029e095
cereallarceny <me@patrickcason.com>
2019-09-04 19:17:33 +0100

    Shutting down bad TCP connections

--- src/index.js
+++ src/index.js
@@ -5,35 +5,38 @@ import { Logger } from 'syft-helpers.js';
 
 import start from './socket';
 
-const DEFAULT_REDIS_URL = 'redis://127.0.0.1:6379';
-const DEFAULT_MONGO_URL = 'mongodb://localhost:27017/grid';
+// Define the port
+const PORT = process.env.PORT || 3000;
 
-// Define the socket port and create the Websocket server
-const port = process.env.PORT || 3000;
-const wss = new WebSocket.Server({ port });
+// Define our Redis and MongoDB urls
+const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/grid';
 
-// Define the Redis port and create the pub/sub clients
-const redisUrl = process.env.REDIS_URL || DEFAULT_REDIS_URL;
-const pub = Redis.createClient(redisUrl);
-const sub = Redis.createClient(redisUrl);
-
-// Define the MongoDB connection url
-const mongoUrl = process.env.MONGODB_URI || DEFAULT_MONGO_URL;
+// Define detailed information about MongoDB
 const mongoDatabase = mongoUrl.substring(mongoUrl.lastIndexOf('/') + 1);
 export const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 };
-const mongo = new MongoClient(mongoUrl, mongoOptions);
 
-// Define the Logger
-const logger = new Logger('grid.js', process.env.VERBOSE);
+// Create our MongoDB client with this information
+const mongo = new MongoClient(mongoUrl, mongoOptions);
 
 // Connect to the DB
 mongo.connect().then(() => {
   // Select the DB we connected to if we have one, otherwise, use 'grid'
   const db = mongo.db(mongoDatabase);
 
+  // Create the WebSocket Server
+  const wss = new WebSocket.Server({ port: PORT });
+
+  // Define the Redis port and create the pub/sub clients
+  const pub = Redis.createClient(redisUrl);
+  const sub = Redis.createClient(redisUrl);
+
+  // Define the Logger
+  const logger = new Logger('grid.js', process.env.VERBOSE);
+
   // Start the server officially, sending in the database and everything else we need
-  start(db, wss, pub, sub, logger, port);
+  start(db, wss, pub, sub, logger, PORT);
 });
--- test/socket.test.js
+++ test/socket.test.js
@@ -1,3 +1,84 @@
+import { Logger, GET_PLANS } from 'syft-helpers.js';
+import { Server, WebSocket } from 'mock-socket';
+import Redis from 'redis-mock';
+import { MongoClient } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import { mongoOptions } from '../src';
+import runSockets from '../src/socket';
+
+class FakeClient {
+  constructor(url) {
+    this.messages = [];
+    this.connection = new WebSocket(url);
+
+    this.connection.onmessage = event => {
+      this.messages.push(event.data);
+    };
+  }
+
+  sendMessage(message) {
+    this.connection.send(JSON.stringify(message));
+  }
+}
+
 describe('Socket', () => {
-  test('it can initialize', () => {});
+  const port = 3001;
+  const url = `ws://localhost:${port}`;
+  const logger = new Logger('grid.js', true);
+  const wss = new Server(url);
+  const client = new FakeClient(url);
+
+  let mongoServer, connection, db, pub, sub, sockets;
+
+  beforeAll(async () => {
+    mongoServer = new MongoMemoryServer();
+
+    const mongoUrl = await mongoServer.getConnectionString();
+    const mongoDatabase = await mongoServer.getDbName();
+
+    connection = await MongoClient.connect(mongoUrl, mongoOptions);
+    db = connection.db(mongoDatabase);
+
+    db.collection('protocols').insertOne({
+      id: 'multiple-millionaire-problem',
+      plans: [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3'], ['c1', 'c2', 'c3']]
+    });
+
+    db.collection('protocols').insertOne({
+      id: 'millionaire-problem',
+      plans: [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']]
+    });
+
+    pub = Redis.createClient();
+    sub = Redis.createClient();
+
+    sockets = runSockets(db, wss, pub, sub, logger, port);
+  });
+
+  afterAll(async () => {
+    await db.dropDatabase();
+
+    if (connection) connection.close();
+    if (mongoServer) await mongoServer.stop();
+
+    db = null;
+
+    pub = null;
+    sub = null;
+
+    sockets = null;
+  });
+
+  test('should initialize', () => {
+    client.connection.onopen = () => {
+      client.sendMessage({
+        type: GET_PLANS,
+        data: { protocolId: 'millionaire-problem' }
+      });
+
+      console.log(client);
+      console.log(client.messages);
+    };
+  });
 });
